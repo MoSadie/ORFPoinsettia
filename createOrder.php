959e7473927d81712d68c6f4c9e4e62e77c30095
Sean Flo <sean@cushmantech.net>
2017-11-06 20:42:04 -0800

    Added favicon

--- createOrder.php
+++ createOrder.php
@@ -34,6 +34,7 @@ sqlsrv_free_stmt($getResults);
     <head>
         <title>Olympia Robotics Poinsettia Sales</title>
         <link rel="stylesheet" href="Home.css"/>
+        <link href="favicon.png" rel="icon">
     </head>
     <body>
         <div style="margin:auto">
