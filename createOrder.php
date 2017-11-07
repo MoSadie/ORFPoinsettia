02bb0efaf679439ec07f7c95be1300bca8ae38af
MoSadie <sean@cushmantech.net>
2017-11-06 16:31:34 -0800

    Note to self: MySQL != SQL

--- createOrder.php
+++ createOrder.php
@@ -6,20 +6,25 @@ if (!isSet($_POST)) {
 
 include "scripts/config.php";
 
-$db_conn = new mysqui($db_hostname,$db_hostname,$db_password);
+$connectionOptions = array(
+    "Database" => $db_database,
+    "Uid" => $db_username,
+    "PWD" => $db_password
+);
 
-if ($db_conn->connect_error) {
-    die("Connection failed: " . $db_conn->connect_error);
-} 
+
+$db_conn = sqlsrv_connect($serverName, $connectionOptions);
 
 $query = "INSERT INTO Orders (Red6, White6, Pink6, Red8Half, White8Half, Pink8Half, Red10Pot, White10Pot, Pink10Pot, Red10PotHanging, White10PotHanging, Pink10PotHanging, Red10Centerpiece, White10Centerpiece, Pink10Centerpiece, SpecialInstructions, DeliveryDate, PayOnDelivery, FirstName, LastName, AddressLine1, AddressLine2, City, State, ZipCode, PhoneNumber, Email, DeliveryInstructions, Salesperson)
 VALUES ('".$_POST["SixRed"]."','".$_POST["SixWhite"]."','".$_POST["SixPink"]."','".$_POST["EightPointFiveRed"]."','".$_POST["EightPointFiveWhite"]."','".$_POST["EightPointFivePink"]."','".$_POST["TenRed"]."','".$_POST["TenWhite"]."','".$_POST["TenPink"]."','".$_POST["TenHangingRed"]."','".$_POST["TenHangingWhite"]."','".$_POST["TenHangingPink"]."','".$_POST["TenCenterRed"]."','".$_POST["TenCenterWhite"]."','".$_POST["TenCenterPink"]."','".$_POST["SpecialOrderInstructions"]."','".$_POST["DeliveryDate"]."','".($_POST["Payment"] == "On Delivery" ? "1" : "0")."','".$_POST["FirstName"]."','".$_POST["LastName"]."','".$_POST["Address1"]."','".$_POST["Address2"]."','".$_POST["City"]."','".$_POST["State"]."','".$_POST["ZipCode"]."','".$_POST["PhoneNumber"]."','".$_POST["Email"]."','".$_POST["DeliveryInstructions"]."','".$_POST["SalesPerson"]."');";
 
-if ($db_conn->query($sql) === TRUE) {
+$getResults= sqlsrv_query($db_conn, $query);
+
+if ($getResults != false) {
     echo "New record created successfully";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sql . "<br>" . sqlsrv_errors();
 }
 
-$db_conn->close();
+sqlsrv_free_stmt($getResults);
 ?>
